c68adbaf4e6dc406ed2d17f022144effcb4c6362
Paige Niedringhaus <paigen11@gmail.com>
2019-01-06 13:01:45 -0500

    more reduxing with react

---
+++ src/js/components/List.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+// the key for connecting a React component with Redux is connect.
+// since we want a list of articles, it's a matter of
+// connecting state.articles with the component. How? With mapStateToProps
+// Always remember: the state in redux comes from reducers.
+
+const mapStateToProps = state => {
+  return { articles: state.articles };
+};
+
+const ConnectedList = ({ articles }) => (
+  <ul className="list-group list-group-flush">
+    {articles.map(el => (
+      <li className="list-group-item" key={el.id}>
+        {el.title}
+      </li>
+    ))}
+  </ul>
+);
+
+const List = connect(mapStateToProps)(ConnectedList);
+
+export default List;
